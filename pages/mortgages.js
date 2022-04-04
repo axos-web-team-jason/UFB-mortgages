0d89c13b85fe0984614b60717d120a5c39b2b715
Jason Songvilay <JSongvilay@bofifederalbank.com>
2022-04-04 15:28:17 -0700

    css modules removal

--- pages/mortgages.js
+++ pages/mortgages.js
@@ -9,7 +9,6 @@ import SecondaryBlockContainer from "../containers/SecondaryBlockContainer/Secon
 import MortgageLayout from "../components/Layout/MortgageLayout";
 import mortgagesPage from "../data/mortgagesPage";
 import Lucky from "../assets/lucky-promo-LP.png";
-import styles from "./Mortgages.module.css";
 
 export default function Mortgages({ mortgages }) {
   // const { heroBannerHeader } = mortgages[0].fields; // access the data fields from Contentful
@@ -25,25 +24,23 @@ export default function Mortgages({ mortgages }) {
         link={mortgagesPage.heroBanner.buttonLink}
       />
       <div className="grayBg section">
-        <h1 className={`${styles.twoUpHeader} centeredHeader`}>
+        <h1 className={`twoUpHeader centeredHeader`}>
           {mortgagesPage.twoUpHeader}
         </h1>
         <TwoUpBlockContainer blocks={mortgagesPage.twoUpBlock} />
       </div>
       <div className="section">
-        <h1 className={`${styles.mainContentHeader} centeredHeader`}>
+        <h1 className={`mainContentHeader centeredHeader`}>
           {mortgagesPage.mainContent.pageHeader}
         </h1>
         <MainBlockContainer block={mortgagesPage.mainContent} img={Lucky} />
       </div>
       <CTA content={mortgagesPage.CTA} />
       <div className="section">
-        <h1 className={`${styles.mainContentHeader} centeredHeader`}>
+        <h1 className={`mainContentHeader centeredHeader`}>
           {mortgagesPage.tertBlockHeader}
         </h1>
-        <div className={`${styles.sectionBody} `}>
-          {mortgagesPage.tertTestimonial}
-        </div>
+        <div className={`sectionBody `}>{mortgagesPage.tertTestimonial}</div>
         <TertiaryBlockContainer blocks={mortgagesPage.tile} />
       </div>
       <Articles />
